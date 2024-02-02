7844978b954379b1f567e8aed1a13a12b3816387
theludway <theludway@gmail.com>
2024-02-02 14:59:18 -0500

    Se completó hasta el ejercicio 5

--- app/src/main/java/com/example/ejercicios/Problema3.kt
+++ app/src/main/java/com/example/ejercicios/Problema3.kt
@@ -1,6 +1,5 @@
 package com.example.ejercicios
 
-import kotlin.math.pow
 import kotlin.math.round
 
 // Nombre: Ludwig Alvarado Becerra
@@ -14,7 +13,16 @@ fun main() {
 
 // Función que debe desarrollar la lógica para la solución del problema
 fun problema3() {
-    // Desarrolle aquí la lógica
+    /*
+    Programa que imprime en consola el promedio de los tres cortes de una asignatura
+    Input:
+        materia:    String, asignatura la cual se está evaluando
+        c1:         Float, nota que pertenece al primer corte
+        c2:         Float, nota que pertenece al segundo corte
+        c3:         Float, nota que pertenece al último corte
+    Output:
+        Float: c1*33% + c2*33% + c3*34% Resultado redondeado a 3 dígitos
+     */
     println("Ingresa la asignatura:")
     val materia = readln()
 
@@ -29,7 +37,9 @@ fun problema3() {
 
     println("Asignatura: $materia")
 
-    var definitiva = round((c1*0.333 + c2*0.333 + c3*0.334) * Math.pow(10.0, 3.0)) / Math.pow(10.0, 3.0)
+    // Se múltiplica por 1000 para luego ser redondeado al entero más cercano y luego
+    // dividir entre 1000 para que la salida sea un flotante
+    var definitiva = round((c1*0.333 + c2*0.333 + c3*0.334) * 1000) / 1000
 
     println("Definitiva: $definitiva")
 }
