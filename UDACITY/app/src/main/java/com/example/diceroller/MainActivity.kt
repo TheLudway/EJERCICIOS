a52c0e12b678825700784c3e478865f95176fd4f
Ludwig <theludway@gmail.com>
2024-03-24 21:49:37 -0500

    Se acabó la primera lección, se culminó:
    1. Inicializar Android Studio
    2. Inicializar en el dispositivo.
    3. Estructura de una aplicación básica.
    4. Layouts, actividades e inflación.
    5. Interacción via botones.
    6. Gradle y compatibilidad para Android.

--- UDACITY/app/src/main/java/com/example/diceroller/MainActivity.kt
+++ UDACITY/app/src/main/java/com/example/diceroller/MainActivity.kt
@@ -1,6 +1,9 @@
 package com.example.diceroller
 
 import android.os.Bundle
+import android.widget.ImageView
+import android.widget.TextView
+import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
@@ -13,13 +16,34 @@ import androidx.compose.ui.tooling.preview.Preview
 import com.example.diceroller.R.layout
 import com.example.diceroller.databinding.LayoutBinding
 import com.example.diceroller.ui.theme.DiceRollerTheme
+import kotlin.random.Random as Random
 
 class MainActivity : ComponentActivity() {
     private lateinit var binding: LayoutBinding
+    lateinit var diceImage: ImageView
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = LayoutBinding.inflate(layoutInflater)
         setContentView(binding.root)
-        binding.rollButton.text = "Vamos a girar!"
+        binding.rollButton.text = "Presionar para girar!"
+        binding.rollButton.setOnClickListener {
+            rollDice()
+        }
+
+        diceImage = binding.diceImage
+
+    }
+
+    private fun rollDice() {
+        val randomInt = Random.nextInt(6) + 1
+        val drawableResource = when (randomInt){
+            1 -> R.drawable.dice_1
+            2 -> R.drawable.dice_2
+            3 -> R.drawable.dice_3
+            4 -> R.drawable.dice_4
+            5 -> R.drawable.dice_5
+            else -> R.drawable.dice_6
+        }
+        diceImage.setImageResource(drawableResource)
     }
 }
