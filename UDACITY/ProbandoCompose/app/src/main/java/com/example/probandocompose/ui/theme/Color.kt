17009172c2995ef6897e4598a7e216a630e048ac
Ludwig <theludway@gmail.com>
2024-04-11 16:57:12 -0500

    Commit inicial

---
+++ UDACITY/ProbandoCompose/app/src/main/java/com/example/probandocompose/ui/theme/Color.kt
@@ -0,0 +1,14 @@
+package com.example.probandocompose.ui.theme
+
+import androidx.compose.ui.graphics.Color
+
+val Purple80 = Color(0xFF4C02FA)
+val PurpleGrey80 = Color(0xFFCCC2DC)
+val Pink80 = Color(0xFFEFB8C8)
+
+val Purple40 = Color(54, 0, 140, 255)
+val PurpleGrey40 = Color(0xFF625b71)
+val Pink40 = Color(0xFF7D5260)
+
+val White = Color.White
+val Black = Color.Black
